817fd1835852775acd593db3ff54f4451a16eb6b
Richard Gooch <rg+github0@safe-mbox.com>
2026-04-06 19:45:21 -0700

    cmd/imageserver: add -informationDatabaseTemplate option.

--- cmd/imageserver/main.go
+++ cmd/imageserver/main.go
@@ -39,6 +39,8 @@ var (
 	imageServerPortNum = flag.Uint("imageServerPortNum",
 		constants.ImageServerPortNumber,
 		"Port number of image server")
+	informationDatabaseTemplate = flag.String("informationDatabaseTemplate", "",
+		"Optional template for generating HTML links into external information database")
 	lockCheckInterval = flag.Duration("lockCheckInterval", 2*time.Second,
 		"Interval between checks for lock timeouts")
 	lockLogTimeout = flag.Duration("lockLogTimeout", 5*time.Second,
@@ -147,8 +149,9 @@ func main() {
 	logger.Printf("Service ready, opening listener on port: %d\n", *portNum)
 	err = httpd.StartServer(
 		httpd.Config{
-			AllowUnauthenticatedReads: *allowUnauthenticatedReads,
-			PortNumber:                *portNum,
+			AllowUnauthenticatedReads:   *allowUnauthenticatedReads,
+			InformationDatabaseTemplate: *informationDatabaseTemplate,
+			PortNumber:                  *portNum,
 		},
 		httpd.Params{
 			ImageDataBase: imdb,
