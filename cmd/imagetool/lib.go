rgooch/Dominator

commit 6224e71c3d7a03cac57c7da887f4a44a39412694

cmd/imagetool: show-metadata: show information database URL.

cmd/imagetool/lib.go
@@ -48,14 +48,15 @@ const (
 )
 
 type typedImage struct {
-	buildLog   *image.Annotation
-	fileSystem *filesystem.FileSystem
-	filter     *filter.Filter
-	image      *image.Image
-	imageName  string
-	imageType  uint
-	specifier  string
-	triggers   *triggers.Triggers
+	buildLog               *image.Annotation
+	fileSystem             *filesystem.FileSystem
+	filter                 *filter.Filter
+	image                  *image.Image
+	imageName              string
+	imageType              uint
+	informationDatabaseURL string
+	specifier              string
+	triggers               *triggers.Triggers
 }
 
 type readCloser struct {
@@ -231,19 +232,19 @@ func getTypedImageFilter(typedName string) (*filter.Filter, error) {
 	return filt, nil
 }
 
-func getTypedImageMetadata(typedName string) (*image.Image, error) {
+func getTypedImageMetadata(typedName string) (*image.Image, string, error) {
 	ti, err := makeTypedImage(typedName)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	if err := ti.loadMetadata(); err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	img, err := ti.getImage()
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	return img, nil
+	return img, ti.informationDatabaseURL, nil
 }
 
 func getTypedImageTriggers(typedName string) (*triggers.Triggers, error) {
@@ -370,11 +371,12 @@ func (ti *typedImage) load() error {
 		ti.fileSystem = fs
 	case imageTypeImage:
 		imageSClient, _ := getClients()
-		img, err := getImage(imageSClient, ti.specifier)
+		img, informationDatabaseURL, err := getImage(imageSClient, ti.specifier)
 		if err != nil {
 			return err
 		}
 		ti.buildLog = img.BuildLog
+		ti.informationDatabaseURL = informationDatabaseURL
 		ti.fileSystem = img.FileSystem
 		ti.filter = img.Filter
 		ti.image = img
@@ -382,11 +384,13 @@ func (ti *typedImage) load() error {
 		ti.triggers = img.Triggers
 	case imageTypeLatestImage:
 		imageSClient, _ := getClients()
-		img, name, err := getLatestImage(imageSClient, ti.specifier, false)
+		img, informationDatabaseURL, name, err :=
+			getLatestImage(imageSClient, ti.specifier, false)
 		if err != nil {
 			return err
 		}
 		ti.buildLog = img.BuildLog
+		ti.informationDatabaseURL = informationDatabaseURL
 		ti.fileSystem = img.FileSystem
 		ti.filter = img.Filter
 		ti.image = img
@@ -423,21 +427,24 @@ func (ti *typedImage) load() error {
 func (ti *typedImage) loadMetadata() error {
 	switch ti.imageType {
 	case imageTypeImage:
-		img, err := getImageMetadata(ti.specifier)
+		img, informationDatabaseURL, err := getImageMetadata(ti.specifier)
 		if err != nil {
 			return err
 		}
 		ti.buildLog = img.BuildLog
+		ti.informationDatabaseURL = informationDatabaseURL
 		ti.filter = img.Filter
 		ti.image = img
 		ti.triggers = img.Triggers
 	case imageTypeLatestImage:
 		imageSClient, _ := getClients()
-		img, name, err := getLatestImage(imageSClient, ti.specifier, true)
+		img, informationDatabaseURL, name, err := getLatestImage(
+			imageSClient, ti.specifier, true)
 		if err != nil {
 			return err
 		}
 		ti.buildLog = img.BuildLog
+		ti.informationDatabaseURL = informationDatabaseURL
 		ti.filter = img.Filter
 		ti.image = img
 		ti.imageName = name
@@ -572,26 +579,32 @@ func findHypervisorClient(client *srpc.Client,
 	return reply.HypervisorAddress, nil
 }
 
-func getImage(client *srpc.Client, name string) (*image.Image, error) {
-	img, err := imgclient.GetImageWithTimeout(client, name, *timeout)
+func getImage(client *srpc.Client, name string) (*image.Image, string, error) {
+	request := img_proto.GetImageRequest{
+		ImageName: name,
+		Timeout:   *timeout,
+	}
+	var reply img_proto.GetImageResponse
+	err := client.RequestReply("ImageServer.GetImage", request, &reply)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	if img == nil {
-		return nil, errors.New(name + ": not found")
+	if reply.Image == nil {
+		return nil, "", fmt.Errorf("image: %s not found", name)
 	}
+	img := reply.Image
 	if err := img.FileSystem.RebuildInodePointers(); err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	if img.Filter != nil {
 		if err := img.Filter.Compile(); err != nil {
-			return nil, err
+			return nil, "", err
 		}
 	}
-	return img, nil
+	return img, reply.InformationDatabaseURL, nil
 }
 
-func getImageMetadata(imageName string) (*image.Image, error) {
+func getImageMetadata(imageName string) (*image.Image, string, error) {
 	imageSClient, _ := getClients()
 	logger.Debugf(0, "getting image: %s\n", imageName)
 	request := img_proto.GetImageRequest{
@@ -602,16 +615,16 @@ func getImageMetadata(imageName string) (*image.Image, error) {
 	var reply img_proto.GetImageResponse
 	err := imageSClient.RequestReply("ImageServer.GetImage", request, &reply)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	if reply.Image == nil {
-		return nil, fmt.Errorf("image: %s not found", imageName)
+		return nil, "", fmt.Errorf("image: %s not found", imageName)
 	}
-	return reply.Image, nil
+	return reply.Image, reply.InformationDatabaseURL, nil
 }
 
 func getLatestImage(client *srpc.Client, name string,
-	ignoreFilesystem bool) (*image.Image, string, error) {
+	ignoreFilesystem bool) (*image.Image, string, string, error) {
 	imageName, err := imgclient.FindLatestImageReq(client,
 		img_proto.FindLatestImageRequest{
 			BuildCommitId:        *buildCommitId,
@@ -619,20 +632,20 @@ func getLatestImage(client *srpc.Client, name string,
 			IgnoreExpiringImages: *ignoreExpiring,
 		})
 	if err != nil {
-		return nil, "", err
+		return nil, "", "", err
 	}
 	if ignoreFilesystem {
-		img, err := getImageMetadata(imageName)
+		img, informationDatabaseURL, err := getImageMetadata(imageName)
 		if err != nil {
-			return nil, "", err
+			return nil, "", "", err
 		}
-		return img, imageName, nil
+		return img, imageName, informationDatabaseURL, nil
 	}
-	img, err := getImage(client, imageName)
+	img, informationDatabaseURL, err := getImage(client, imageName)
 	if err != nil {
-		return nil, "", err
+		return nil, "", "", err
 	} else {
-		return img, imageName, nil
+		return img, imageName, informationDatabaseURL, nil
 	}
 }
 

cmd/imagetool/showImageMetadata.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Cloud-Foundations/Dominator/lib/image"
 	"github.com/Cloud-Foundations/Dominator/lib/json"
 	"github.com/Cloud-Foundations/Dominator/lib/log"
 )
+
+type imageData struct {
+	ExternalInformationURL string
+	*image.Image
+}
 
 func showImageMetadataSubcommand(args []string, logger log.DebugLogger) error {
 	if err := showImageMetadata(args[0]); err != nil {
@@ -16,11 +22,15 @@ func showImageMetadataSubcommand(args []string, logger log.DebugLogger) error {
 }
 
 func showImageMetadata(imageName string) error {
-	if img, err := getTypedImageMetadata(imageName); err != nil {
+	if img, infoURL, err := getTypedImageMetadata(imageName); err != nil {
 		return err
 	} else if img == nil {
 		return fmt.Errorf("no image")
 	} else {
-		return json.WriteWithIndent(os.Stdout, "    ", img)
+		data := imageData{
+			ExternalInformationURL: infoURL,
+			Image:                  img,
+		}
+		return json.WriteWithIndent(os.Stdout, "    ", data)
 	}
 }

cmd/imagetool/copyFilteredFiles.go
@@ -25,7 +25,7 @@ func copyFilteredFiles(imageName, sourceDirectory, destDirectory string,
 	if err != nil {
 		return err
 	}
-	img, err := getTypedImageMetadata(imageName)
+	img, _, err := getTypedImageMetadata(imageName)
 	if err != nil {
 		return err
 	}

cmd/imagetool/getImageArchiveData.go
@@ -23,7 +23,7 @@ func getImageArchiveDataSubcommand(args []string,
 }
 
 func getImageArchiveDataAndWrite(name, outputFilename string) error {
-	img, err := getTypedImageMetadata(name)
+	img, _, err := getTypedImageMetadata(name)
 	if err != nil {
 		return err
 	}

cmd/imagetool/scanFilteredFiles.go
@@ -24,7 +24,7 @@ func scanFilteredFiles(imageName, dirName string,
 	if err != nil {
 		return err
 	}
-	img, err := getTypedImageMetadata(imageName)
+	img, _, err := getTypedImageMetadata(imageName)
 	if err != nil {
 		return err
 	}

cmd/imagetool/traceInodeHistory.go
@@ -51,7 +51,7 @@ func traceInodeHistory(imageName, inodePath string) error {
 			fmt.Printf("Inode originated in: %s\n", lastImageName)
 			return listInode(lastInode, inodePath, lastNumLinks)
 		}
-		img, err = getImage(imageSClient, sourceImageName)
+		img, _, err = getImage(imageSClient, sourceImageName)
 		if err != nil {
 			return err
 		}

commit 56921733a255f2f0ee35b75a8de1a9845f2f6476

cmd/imagetool: fix bug introduced in 6224e71c3d7a.

cmd/imagetool/lib.go
@@ -384,7 +384,7 @@ func (ti *typedImage) load() error {
 		ti.triggers = img.Triggers
 	case imageTypeLatestImage:
 		imageSClient, _ := getClients()
-		img, informationDatabaseURL, name, err :=
+		img, name, informationDatabaseURL, err :=
 			getLatestImage(imageSClient, ti.specifier, false)
 		if err != nil {
 			return err
@@ -438,7 +438,7 @@ func (ti *typedImage) loadMetadata() error {
 		ti.triggers = img.Triggers
 	case imageTypeLatestImage:
 		imageSClient, _ := getClients()
-		img, informationDatabaseURL, name, err := getLatestImage(
+		img, name, informationDatabaseURL, err := getLatestImage(
 			imageSClient, ti.specifier, true)
 		if err != nil {
 			return err

cmd/imagetool/showImageMetadata.go
@@ -10,7 +10,7 @@ import (
 )
 
 type imageData struct {
-	ExternalInformationURL string
+	InformationDatabaseURL string `json:",omitempty"`
 	*image.Image
 }
 
@@ -28,7 +28,7 @@ func showImageMetadata(imageName string) error {
 		return fmt.Errorf("no image")
 	} else {
 		data := imageData{
-			ExternalInformationURL: infoURL,
+			InformationDatabaseURL: infoURL,
 			Image:                  img,
 		}
 		return json.WriteWithIndent(os.Stdout, "    ", data)