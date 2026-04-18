rgooch/Dominator

commit 2ca7022082bcdb09fc922b51857461a51259e0d1

lib/mbr: add Write() function.

lib/mbr/impl.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/Cloud-Foundations/Dominator/lib/types"
 )
+
+type sizeMiB uint64
 
 func decode(file *os.File) (*Mbr, error) {
 	var mbr Mbr
@@ -31,27 +35,42 @@ func write32LE(address []byte, value uint64) {
 	address[3] = byte((value >> 24) & 0xff)
 }
 
-func writeDefault(filename string, tableType TableType) error {
+func write(filename string, tableType TableType, partitions []Partition) error {
+	if len(partitions) < 1 {
+		partitions = []Partition{{Type: "ext2"}}
+	}
 	label, err := tableType.lookupString()
 	if err != nil {
 		return err
 	}
 	fmt.Printf("making table type: %d (%s)\n", tableType, label)
-	var cmd *exec.Cmd
+	cmd := exec.Command("parted", "-s", "-a", "optimal", filename,
+		"mklabel", label)
+	partitionStart := sizeMiB(1)
 	switch tableType {
 	case TABLE_TYPE_GPT:
-		cmd = exec.Command("parted", "-s", "-a", "optimal", filename,
-			"mklabel", label,
-			"mkpart", "fat32", "1MiB", "129MiB",
-			"mkpart", "primary", "ext2", "129MiB", "100%",
-			"set", "1", "esp", "on",
-		)
+		cmd.Args = append(cmd.Args, "mkpart", "fat32", "1MiB", "129MiB")
+		partitionStart += 128
 	default:
-		cmd = exec.Command("parted", "-s", "-a", "optimal", filename,
-			"mklabel", label,
-			"mkpart", "primary", "ext2", "1", "100%",
-			"set", "1", "boot", "on",
-		)
+	}
+	for _, partition := range partitions {
+		partitionSize := makeSizeMiB(partition.Size)
+		partitionType := partition.Type
+		switch partitionType {
+		case "ext4":
+			partitionType = "ext2"
+		default:
+			partitionType = "ext2"
+		}
+		cmd.Args = append(cmd.Args, "mkpart", "primary", partitionType,
+			partitionStart.String(), (partitionStart + partitionSize).String())
+		partitionStart += partitionSize
+	}
+	switch tableType {
+	case TABLE_TYPE_GPT:
+		cmd.Args = append(cmd.Args, "set", "1", "esp", "on")
+	default:
+		cmd.Args = append(cmd.Args, "set", "1", "boot", "on")
 	}
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -111,4 +130,20 @@ func (mbr *Mbr) write(filename string) error {
 		}
 		return nil
 	}
+}
+
+func makeSizeMiB(size types.Bytes) sizeMiB {
+	unitSize := types.Bytes(1 << 20)
+	sizeInUnits := size / unitSize
+	if sizeInUnits*unitSize < size {
+		sizeInUnits++
+	}
+	return sizeMiB(sizeInUnits)
+}
+
+func (size sizeMiB) String() string {
+	if size < 1 {
+		return "100%"
+	}
+	return fmt.Sprintf("%dMiB", size)
 }

lib/mbr/api.go
@@ -2,6 +2,8 @@ package mbr
 
 import (
 	"os"
+
+	"github.com/Cloud-Foundations/Dominator/lib/types"
 )
 
 const (
@@ -20,6 +22,11 @@ const (
 
 type Mbr struct {
 	raw [512]byte
+}
+
+type Partition struct {
+	Size types.Bytes // Zero: use all remaining space.
+	Type string
 }
 
 type TableType uint
@@ -66,5 +73,11 @@ func (tt TableType) String() string {
 // GPT: a UEFI partition followed by an ext2 partition are created.
 // Other partition types have unspecified behaviour.
 func WriteDefault(filename string, tableType TableType) error {
-	return writeDefault(filename, tableType)
+	return Write(filename, tableType, nil)
+}
+
+// Write will write a partition table of the specified table type with the
+// specified partitions.
+func Write(filename string, tableType TableType, partitions []Partition) error {
+	return write(filename, tableType, partitions)
 }

commit 7ade367dfc9d08ffd46e263a2454c0d754560cc4

lib/mbr: fix bug introduced in 2ca7022082bcd.

lib/mbr/impl.go
@@ -53,9 +53,15 @@ func write(filename string, tableType TableType, partitions []Partition) error {
 		partitionStart += 128
 	default:
 	}
-	for _, partition := range partitions {
+	for index, partition := range partitions {
 		partitionSize := makeSizeMiB(partition.Size)
 		partitionType := partition.Type
+		var partitionEnd sizeMiB
+		if partitionSize > 0 {
+			partitionEnd = partitionStart + partitionSize
+		} else if index != len(partitions)-1 {
+			return fmt.Errorf("only last partition may have unspecified size")
+		}
 		switch partitionType {
 		case "ext4":
 			partitionType = "ext2"
@@ -63,7 +69,7 @@ func write(filename string, tableType TableType, partitions []Partition) error {
 			partitionType = "ext2"
 		}
 		cmd.Args = append(cmd.Args, "mkpart", "primary", partitionType,
-			partitionStart.String(), (partitionStart + partitionSize).String())
+			partitionStart.String(), partitionEnd.String())
 		partitionStart += partitionSize
 	}
 	switch tableType {
@@ -74,8 +80,8 @@ func write(filename string, tableType TableType, partitions []Partition) error {
 	}
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("error partitioning: %s: %s: %s",
-			filename, err, output)
+		return fmt.Errorf("error partitioning (%v): %s: %s: %s",
+			cmd.Args, filename, err, output)
 	}
 	return nil
 }